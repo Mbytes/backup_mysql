408798657724e1385a5595d68a9f4d19eab7f238
Julio_Garcia <julio@mbytes.net>
2015-02-10 17:41:40 +0100

    Actualizacion inicial

---
+++ bbdd_repair.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+#Reparar Tablas BBDD
+
+#Prefijo Tablas
+PREFIX=$1
+
+#Ficheros temporales
+TEMPTABLES=/tmp/databases.sql
+TMPRUN=/tmp/tablesrepair.sh
+TMPDUMP=/tmp/dumpdatabases.sh
+TMPALLRUN=/tmp/ALLtablesrepair.sh
+
+USER=$(cat /etc/mysql/debian.cnf | grep ^user | tail -1 | awk '{print $3}')
+PWD=$(cat /etc/mysql/debian.cnf | grep ^password | tail -1 | awk '{print $3}')
+
+FECHA=$(date +%Y%m%d)
+
+
+#Funcion de tablas por base datos
+#RepairBBDD BASE_DATOS TABLAS
+RepairBBDD ()
+{
+	echo "Reparando $1 "
+	cat $2 | awk '{print "repair table " $1 ";"}' | mysql -u$USER -p$PWD "$1" 
+} #EndFunction
+
+
+
+#Listamos BBDD
+echo show databases | mysql -u$USER -p$PWD | grep -v Database > ${TEMPTABLES} 
+
+
+#Listado de Tablas por BBDD
+cat ${TEMPTABLES} | awk -v USER=$USER -v PWD=$PWD '{print "echo show tables | mysql -u" USER " -p" PWD " \"" $1 "\" |grep -v Tables_in  > /tmp/repair." $1 ".sql"  }' > ${TMPRUN}
+sh ${TMPRUN}
+
+#Procesamos reparar Tablas de cada BBDD
+while read  BBDD
+do
+#	echo "Reparando...  ${BBDD}"
+	RepairBBDD "${BBDD}" /tmp/repair.${BBDD}.sql
+done < ${TEMPTABLES}
+
+
+#Borramos viejos
+rm /home/dump_mysql/*sql
+
+#Realizamos volcados datos
+cat ${TEMPTABLES} | awk -v USER=$USER -v PWD=$PWD -v FECHA=$FECHA -v PREFIX=$PREFIX '{print "mysqldump --compact --insert-ignore --single-transaction -hlocalhost -u" USER " -p" PWD " --quote-names " $1" > /home/dump_mysql/" PREFIX "_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
+sh ${TMPDUMP}
+
+#Realizamos volcados structuras
+cat ${TEMPTABLES} | awk -v USER=$USER -v PWD=$PWD -v FECHA=$FECHA -v PREFIX=$PREFIX '{print "mysqldump -d -hlocalhost -u" USER " -p" PWD " --quote-names " $1" > /home/dump_mysql/" PREFIX "_STRUCT_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
+sh ${TMPDUMP}
+
+
+
+
+##Borrado Final
+exit
