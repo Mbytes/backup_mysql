7e72794ff69dc37740e405041325f04dc40e6b30
Julio_Garcia <julio@mbytes.net>
2015-06-30 20:12:42 +0200

    Actualizacion errores

--- bbdd_repair.sh
+++ bbdd_repair.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #Reparar Tablas BBDD y volcados MySql
 
@@ -6,76 +6,75 @@ DUMP=/home/dump_mysql
 
 HOST=$(cat /etc/hostname | sed 's/\(.*\)/\U\1/')
 
-#Prefijo Tablas
-PREFIX=${HOST}
-
-
-
 #Ficheros temporales
 TEMPTABLES=/tmp/databases.sql
 TMPRUN=/tmp/tablesrepair.sh
 TMPDUMP=/tmp/dumpdatabases.sh
 TMPALLRUN=/tmp/ALLtablesrepair.sh
 
+#Extraemos Usuarios defecto Debian
 USER=$(cat /etc/mysql/debian.cnf | grep ^user | tail -1 | awk '{print $3}')
 PWD=$(cat /etc/mysql/debian.cnf | grep ^password | tail -1 | awk '{print $3}')
 
 FECHA=$(date +%Y%m%d)
 
-
 #Funcion de tablas por base datos
 #RepairBBDD BASE_DATOS TABLAS
-RepairBBDD ()
+function RepairBBDD ()
 {
-	echo "Reparando $1 "
-	cat $2 | awk '{print "repair table " $1 ";"}' | mysql -u$USER -p$PWD "$1" 
+  echo "Reparando $1 "
+  cat "$2" | awk '{print "repair table " $1 ";"}' | mysql -u${USER} -p${PWD} "$1" 
 } #EndFunction
 
 
 #Verificar Existencia Directorios
-function ExisteDirectorio {
+function ExisteDirectorio ()
+{
 if ! test -d $1
 then
   echo "CREANDO DIRECTORIO $1"
-  mkdir $1
+  mkdir "$1"
 fi
+}
 
-ExisteDirectorio ${DUMP}
+ExisteDirectorio "${DUMP}"
 
 #################
 ## PRINCIPAL
 ################
 
 #Listamos BBDD
-echo show databases | mysql -u$USER -p$PWD | grep -v Database > ${TEMPTABLES} 
+echo show "databases" | mysql -u${USER} -p${PWD} | grep -v Database > ${TEMPTABLES} 
 
 
 #Listado de Tablas por BBDD
-cat ${TEMPTABLES} | awk -v USER=$USER -v PWD=$PWD '{print "echo show tables | mysql -u" USER " -p" PWD " \"" $1 "\" |grep -v Tables_in  > /tmp/repair." $1 ".sql"  }' > ${TMPRUN}
-sh ${TMPRUN}
+cat "${TEMPTABLES}" | awk -v USER=${USER} -v PWD=${PWD} '{print "echo show tables | mysql -u" USER " -p" PWD " \"" $1 "\" |grep -v Tables_in  > /tmp/repair." $1 ".sql"  }' > ${TMPRUN}
+sh "${TMPRUN}"
 
 
 #Procesamos reparar Tablas de cada BBDD
 while read  BBDD
 do
 #	echo "Reparando...  ${BBDD}"
-	RepairBBDD "${BBDD}" /tmp/repair.${BBDD}.sql
+	RepairBBDD "${BBDD}" "/tmp/repair.${BBDD}.sql"
 done < ${TEMPTABLES}
 
 
 #Borramos viejos
-rm ${DUMP}/*sql
+rm "${DUMP}/*sql"
 
 #Realizamos volcados datos
-cat ${TEMPTABLES} | awk -v DUMP=${DUMP} -v USER=$USER -v PWD=$PWD -v FECHA=$FECHA -v PREFIX=$PREFIX '{print "mysqldump --compact --insert-ignore --single-transaction -hlocalhost -u" USER " -p" PWD " --quote-names " $1 " -r " DUMP "/" PREFIX "_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
-sh ${TMPDUMP}
+cat "${TEMPTABLES}" | awk -v DUMP=${DUMP} -v USER=${USER} -v PWD=${PWD} -v FECHA=${FECHA} -v PREFIX=${HOST} '{print "mysqldump --compact --insert-ignore --single-transaction -hlocalhost -u" USER " -p" PWD " --quote-names " $1 " -r " DUMP "/" PREFIX "_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
+sh "${TMPDUMP}"
 
 #Realizamos volcados structuras
-cat ${TEMPTABLES} | awk -v DUMP=${DUMP} -v USER=$USER -v PWD=$PWD -v FECHA=$FECHA -v PREFIX=$PREFIX '{print "mysqldump -d -hlocalhost -u" USER " -p" PWD " --quote-names " $1" -r " DUMP "/" PREFIX "_STRUCT_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
-sh ${TMPDUMP}
+cat "${TEMPTABLES}" | awk -v DUMP=${DUMP} -v USER=${USER} -v PWD=${PWD} -v FECHA=${FECHA} -v PREFIX=${HOST} '{print "mysqldump -d -hlocalhost -u" USER " -p" PWD " --quote-names " $1 " -r " DUMP "/" PREFIX "_STRUCT_" $1 "_" FECHA ".sql" }' > ${TMPDUMP}
+sh "${TMPDUMP}"
 
 
 
 
 ##Borrado Final
-exit
+#exit
+
+
